72070336ff1a2a090b6566df722f231d77022f27
tachyonok <tachyonparticle123@gmail.com>
2024-09-12 21:03:55 +0530

    v-else-if ladder

--- vuelearn/src/App.vue
+++ vuelearn/src/App.vue
@@ -3,7 +3,7 @@ export default {
   data() {
     return {
       name: "Tun tun mausi",
-      status: false,
+      status: "active",
     };
   },
 };
@@ -11,6 +11,7 @@ export default {
 
 <template>
   <h1>{{ name }}</h1>
-  <p v-if="status">Active user</p>
-  <p v-else>user is inactive</p>
+  <p v-if="status === 'active'">Active user</p>
+  <p v-else-if="status === 'pending'">Pending user</p>
+  <p v-else>Pending user</p>
 </template>
