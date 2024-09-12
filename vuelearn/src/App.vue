c7811f90a9de8676de0e3d5d15cd4e303cb46742
tachyonok <tachyonparticle123@gmail.com>
2024-09-12 21:08:02 +0530

    lists iteration in vue

--- vuelearn/src/App.vue
+++ vuelearn/src/App.vue
@@ -4,6 +4,7 @@ export default {
     return {
       name: "Tun tun mausi",
       status: "active",
+      tasks: ["Task one", "Task Two", "Task three"],
     };
   },
 };
@@ -14,4 +15,10 @@ export default {
   <p v-if="status === 'active'">Active user</p>
   <p v-else-if="status === 'pending'">Pending user</p>
   <p v-else>Pending user</p>
+
+  <h1>Tasks</h1>
+
+  <ul>
+    <li v-for="task in tasks" :key="task">{{ task }}</li>
+  </ul>
 </template>
