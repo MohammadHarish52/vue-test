04f674d2fd04590f28e4350f722c8e399d991908
tachyonok <tachyonparticle123@gmail.com>
2024-09-12 21:01:32 +0530

    starting with vue js

---
+++ vuelearn/src/App.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+  data() {
+    return {
+      name: "Tun tun mausi",
+      status: false,
+    };
+  },
+};
+</script>
+
+<template>
+  <h1>{{ name }}</h1>
+  <p v-if="status">Active user</p>
+  <p v-else>user is inactive</p>
+</template>
